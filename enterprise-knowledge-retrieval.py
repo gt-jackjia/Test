df9dfb29e3318a43f3fe95df1c5744341f1b32b2
Jack Jia <Jack.Jia@us.gt.com>
2023-06-20 10:38:50 -0700

    first commit

---
+++ enterprise-knowledge-retrieval.py
@@ -0,0 +1,33 @@
+from ast import literal_eval
+import concurrent
+import openai
+import os
+import numpy as np
+from numpy import array, average
+import pandas as pd
+from tenacity import retry, wait_random_exponential, stop_after_attempt
+import tiktoken
+from tqdm import tqdm
+from typing import List, Iterator
+#import wget
+import urllib.request
+
+# Redis imports
+from redis import Redis as r
+from redis.commands.search.query import Query
+from redis.commands.search.field import (
+    TextField,
+    VectorField,
+    NumericField
+)
+from redis.commands.search.indexDefinition import (
+    IndexDefinition,
+    IndexType
+)
+
+# Langchain imports
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains import RetrievalQA
+
+CHAT_MODEL = "gpt-3.5-turbo"
+pd.set_option('display.max_colwidth', 0)
